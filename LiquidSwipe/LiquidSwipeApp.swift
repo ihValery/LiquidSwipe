099129cb9f12fca3fdfd1a29b89529a7a7890e01
ihValery <ihValery@gmail.com>
2021-11-04 00:50:14 +0300

    Initial Commit

---
+++ LiquidSwipe/LiquidSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidSwipeApp.swift
+//  LiquidSwipe
+//
+//  Created by Валерий Игнатьев on 04.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct LiquidSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
